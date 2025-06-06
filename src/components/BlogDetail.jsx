ee4d22aafcd46154e6a8a0e7e9a9096444baf6cd
6bilguun9 <batsukhbilguun999@gmail.com>
2025-06-06 17:26:22 -0400

    first blog

--- src/components/BlogDetail.jsx
+++ src/components/BlogDetail.jsx
@@ -38,8 +38,8 @@ const BlogDetail = () => {
   if (!post) return <div>Post not found</div>;
 
   return (
-    <div className="p-5 min-h-screen mx-auto bg-neutral-950 text-neutral-200 gap-4 flex flex-col items-center">
-      <div className="w-1/2 flex flex-col text-center">
+    <div className="p-15 min-h-screen mx-auto bg-neutral-950 text-neutral-200 gap-4 flex flex-col items-center">
+      <div className="w-1/2 flex flex-col">
         <Link to="/Blog" className="text-neutral-300 mb-4 block">
           ← Back to all posts
         </Link>
@@ -47,8 +47,38 @@ const BlogDetail = () => {
         <h1 className="text-4xl font-bold">{post.title}</h1>
         <p className="text-sm text-neutral-500 mb-6">{post.date}</p>
 
-        <div className="prose prose-invert text-neutral-200 text-lg">
-          <ReactMarkdown>{post.content}</ReactMarkdown>
+        <div className="prose prose-invert text-neutral-200 text-xl gap-4">
+          <ReactMarkdown
+            components={{
+              img: ({ node, ...props }) => (
+                <img
+                  src={props.src}
+                  alt={props.alt || ""}
+                  className="max-w-full h-auto my-4"
+                />
+              ),
+              p: ({ node, ...props }) => (
+                <p
+                  className="text-lg text-neutral-300 leading-8 mb-6"
+                  {...props}
+                />
+              ),
+              h2: ({ node, ...props }) => (
+                <h2 className="text-3xl font-bold mt-10 mb-4" {...props} />
+              ),
+              h3: ({ node, ...props }) => (
+                <h3 className="text-2xl font-semibold mt-8 mb-3" {...props} />
+              ),
+              ul: ({ node, ...props }) => (
+                <ul
+                  className="list-disc ml-6 mb-6 text-neutral-300 leading-7"
+                  {...props}
+                />
+              ),
+            }}
+          >
+            {post.content}
+          </ReactMarkdown>
         </div>
       </div>
     </div>
